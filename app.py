9666292bde7eeb957bdfc1b9cd1705b80283e028
rtdjin300 <rittwick.dutta@gmail.com>
2023-08-03 17:06:15 -0400

    This commit includes upadting the requirements file and Procfile for Heroku

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from sklearn.preprocessing import StandardScaler
 app=Flask(__name__)
 ## Load the model
 regmodel=pickle.load(open("regmodel.pkl", 'rb'))
+scalar=pickle.load(open("scaling.pkl", 'rb'))
 
 @app.route('/')
 def home():
@@ -19,10 +20,22 @@ def predict_api():
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
     scaler = StandardScaler()
-    new_data=scaler.fit_transform(np.array(list(data.values())).reshape(1,-1))
+    new_data=scalar.transform(np.array(data).reshape(1,-1))
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    scaler = StandardScaler()
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The predicted house price is ${}".format(output))
+
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
